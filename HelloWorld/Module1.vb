661d8cefe607749f1e2ddc441f1069a2ae1dd8a7
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-01 16:08:31 +0300

    Created HelloWorld VBNet Projct
    HelloWorld. VBNet Tutorials. Platform DotNet FrameWork. Console Application

---
+++ HelloWorld/Module1.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello, World!")
+    End Sub
+
+End Module
---
+++ HelloWorld/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("HelloWorld")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HelloWorld")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("fa66878d-ee09-4dfa-99d8-d1a900df186f")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
